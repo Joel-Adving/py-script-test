c0abb1317c63181a7c2ed915379b9a7583c9ad9f
Joel-Adving <joel.adving@gmail.com>
2022-05-29 16:39:37 +0200

    styling, responsiveness and some formatting

--- script.py
+++ script.py
@@ -5,7 +5,7 @@ import sys
 
 inputElement = document.querySelector('#input')
 buttonElement = document.querySelector('#button')
-responseDataElement = document.querySelector('.response-data')
+artistInfoElement = document.querySelector('.artist-info')
 
 async def fetch(url):
     response = await pyfetch(url=url, method="GET")
@@ -13,12 +13,12 @@ async def fetch(url):
     return data
 
 async def get_artist(e):
-    responseDataElement.innerHTML = ""
+    artistInfoElement.innerHTML = ""
     spotify_uri = inputElement.value
 
     error_msg = "Please enter a valid URI like the example above."
     if not "artist:" in spotify_uri or inputElement.value == "":
-        return responseDataElement.insertAdjacentHTML('afterbegin', error_msg)
+        return artistInfoElement.insertAdjacentHTML('afterbegin', error_msg)
 
     spotify_url = 'https://open.spotify.com/artist/' + spotify_uri.split("artist:")[1]
     query_for_mbid = "https://musicbrainz.org/ws/2/url/?query=" + spotify_url + "&fmt=json&limit=1"
@@ -30,7 +30,7 @@ async def get_artist(e):
     artist_data = await fetch(query_artist)
 
     if artist_data["name"] == "Frederick Westcott":
-        return responseDataElement.insertAdjacentHTML('afterbegin', "Artist not found")
+        return artistInfoElement.insertAdjacentHTML('afterbegin', "Artist not found")
 
     artist_alias = []
     artist_genre = []
@@ -49,20 +49,20 @@ async def get_artist(e):
         artist_resource.append(url['resource'])
 
     markup = []
-    markup.append('<p>Name: %s</p>' % artist_data['name'])
-    markup.append('<p>Disambiguation: %s</p>' % artist_data['disambiguation'])
-    markup.append('<p>Type: %s</p>' % artist_data['type'])
-    markup.append('<p>Country: %s</p>' % artist_data['country'])
-    markup.append('<p>Gender: %s</p>' % artist_data['gender'])
-    markup.append('<p>ISNI: %s</p>' % artist_data['isnis'][0])
-    markup.append('<p>IPI: %s</p>' % artist_data['ipis'][0])
-    markup.append('<p>Alias: %s</p>' % artist_alias)
-    markup.append('<p>Genres: %s</p>' % artist_genre)
-    markup.append('<p>Spotify URI: %s</p>' % spotify_uri)
-    markup.append('<p>MusicBrainz ID: %s</p>' % mbid)
-    markup.append('<p>Artist Resource List: %s</p>' % artist_resource)
+    markup.append('<p>Name: <strong> %s </strong> </p>' % artist_data['name'])
+    markup.append('<p>Disambiguation: <strong> %s </strong> </p>' % artist_data['disambiguation'])
+    markup.append('<p>Type: <strong> %s </strong> </p>' % artist_data['type'])
+    markup.append('<p>Country: <strong> %s </strong> </p>' % artist_data['country'])
+    markup.append('<p>Gender: <strong> %s </strong> </p>' % artist_data['gender'])
+    markup.append('<p>ISNI: <strong> %s </strong> </p>' % artist_data['isnis'][0])
+    markup.append('<p>IPI: <strong> %s </strong> </p>' % artist_data['ipis'][0])
+    markup.append('<p>Alias: <strong> %s </strong> </p>' % ', '.join(artist_alias))
+    markup.append('<p>Genres: <strong> %s </strong> </p>' % ', '.join(artist_genre))
+    markup.append('<p>Spotify URI: <strong> %s </strong> </p>' % spotify_uri)
+    markup.append('<p>MusicBrainz ID: <strong> %s </strong> </p>' % mbid)
+    markup.append('<p> <strong> Artist Resource List: </strong> <br> %s </p>' % '<br>'.join(artist_resource))
    
-    responseDataElement.insertAdjacentHTML('afterbegin', ' '.join(markup))
+    artistInfoElement.insertAdjacentHTML('afterbegin', ' '.join(markup))
     inputElement.value = ""
 
 def main():
